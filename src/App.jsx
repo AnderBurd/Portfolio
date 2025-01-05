8d95bc0eee39f586c165336da8e5233595795c35
Ander <[AndersonLee0248@gmail.com]>
2025-01-04 16:53:35 -0800

    Finished link section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Intro from './components/intro.jsx';
 import Projects from './components/projectSection.jsx';
+import Contact from './components/contactMe.jsx';
 import './styling/App.css';
 import React from 'react';
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className = "App">
       <Intro />
       <Projects />
+      <Contact />
     </div>
   );
 };
---
+++ src/components/contactMe.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SectionWrapper from './sectionWrapper.jsx';
+import '../styling/contactMe.css'
+
+
+
+const Contact = () =>{
+    return(
+        <SectionWrapper title = "Links">
+            <div className = "Links">
+                <p>AndersonLee0248@gmail.com</p>
+                <a href="https://github.com/AnderBurd" target="_blank">GitHub <span class="lnr lnr-location"></span> </a>
+                <a href="https://www.linkedin.com/in/anderson-lee-796ab2330/" target="_blank">Linkedin <span class="lnr lnr-location"></span> </a>
+            </div>
+
+        </SectionWrapper>
+    );
+};
+
+
+export default Contact
